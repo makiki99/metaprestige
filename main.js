698d6932f175637d7ff96fb259ccb41896c73b9e
makiki <makiki-the-kobold@proton.me>
2020-04-01 11:41:15 +0200

    meta bonus calculation

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 var data = {
 	coins: 0,
 	prestiges: [0,0,0,0,0,0,0,0,0,0],
-	tokens: 0
+	tokens: 0,
+	multiForOthers: 1
 };
+
+var multiFromOthers = 0;
 
 function getGain() {
 	var gain = 1;
@@ -35,13 +38,14 @@ function activatePrestige(id) {
 				data.prestiges[i] = 0;
 			}
 			data.prestiges[id]++;
+			data.multiForOthers++;
 	}
 	draw();
 }
 
 function update() {
 	data.coins += getGain();
-	localStorage.SHITPOST = JSON.stringify(data);
+	localStorage.META = JSON.stringify(data);
 }
 
 function draw() {
@@ -57,12 +61,50 @@ function draw() {
 			document.getElementById("tier"+(i+1)+"btn").disabled = true;
 		}
 	})
+	document.getElementById("bonus_taken").innerHTML = multiFromOthers;
+	document.getElementById("bonus_given").innerHTML = data.multiForOthers;
+}
+
+function updateMultiFromOthers() {
+	multiFromOthers = 1;
+	if (localStorage.SHITPOST) {
+		let tmp_data = JSON.parse(localStorage.SHITPOST);
+		for (let i = 0; i < tmp_data.prestiges.length; i++) {
+			multiFromOthers += tmp_data.prestiges[i];
+		}
+	}
+	if (localStorage.QUADRATIC_SHITPOST) {
+		let tmp_data = JSON.parse(localStorage.QUADRATIC_SHITPOST);
+		let total = 0;
+		for (let x = 0; i < tmp_data.prestiges.length; i++) {
+			const el = tmp_data.prestiges[x];
+			for (let y = 0; y < el.length; y++) {
+				total += el[y];
+			}
+		}
+		multiFromOthers += total**(1/2)
+	}
+	if (localStorage.OH_NO) {
+		let tmp_data = JSON.parse(localStorage.OH_NO);
+		let total = 0;
+		for (let x = 0; i < tmp_data.prestiges.length; i++) {
+			const el = tmp_data.prestiges[x];
+			for (let y = 0; y < el.length; y++) {
+				for (let z = 0; z < el[y].length; z++) {
+					total +=  el[y][z];
+				}
+			}
+		}
+		multiFromOthers += total**(1/3)
+	}
+	multiFromOthers **= 1/3;
 }
 
 window.addEventListener("load",function () {
-	if (localStorage.SHITPOST) {
-		data = JSON.parse(localStorage.SHITPOST)
+	if (localStorage.META) {
+		data = JSON.parse(localStorage.META);
 	}
+	updateMultiFromOthers();
 	draw();
 	for (var i = 0; i < 10; i++) {
 		document.getElementById("tier"+(i+1)+"btn").addEventListener(
